7137d36b8eca34e27ff3e2f1d756747f68fac274
RicardoBrito1938 <ricardo.jucrist@hotmail.com>
2023-12-12 17:00:52 +0000

    chore: pattern composition

---
+++ src/app/components/Input/index.tsx
@@ -0,0 +1,33 @@
+import { ComponentProps } from 'react'
+
+export interface InputProps {
+  className?: string
+}
+
+type PrefixProps = ComponentProps<'div'>
+
+export function Prefix(props: PrefixProps) {
+  return <div {...props} />
+}
+
+type ConTrolProps = ComponentProps<'input'>
+
+export function Control(props: ConTrolProps) {
+  return (
+    <input
+      className="flex-1 border-0 bg-transparent p-8 text-zinc-900 placeholder-zinc-600"
+      {...props}
+    />
+  )
+}
+
+export type RootProps = ComponentProps<'div'>
+
+export function Root(props: RootProps) {
+  return (
+    <div
+      className="mx-1 flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm"
+      {...props}
+    />
+  )
+}
--- src/app/components/Sidebar/index.tsx
+++ src/app/components/Sidebar/index.tsx
@@ -1,20 +1,19 @@
-import { SearchMd } from '@untitled-ui/icons-react'
 import { MainNavigation } from './MainNavigation'
 import { NavItem } from './NavItem'
-import { Cog, LifeBuoy } from 'lucide-react'
+import { Cog, LifeBuoy, Search } from 'lucide-react'
 import { UsedSpaceWidget } from './UsedSpaceWidget'
 import { Profile } from './Profile'
+import * as Input from '../Input'
 
 export function Sidebar() {
   return (
     <aside className="flex flex-col gap-6 border-r border-zinc-200 px-5 py-8">
-      <div className="flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm">
-        <SearchMd className="h-5 w-5 text-zinc-500" />
-        <input
-          className="w-full bg-transparent focus:outline-none"
-          placeholder="Search"
-        />
-      </div>
+      <Input.Root>
+        <Input.Prefix>
+          <Search className="h-5 w-5 text-zinc-500" />
+        </Input.Prefix>
+        <Input.Control placeholder="Search" />
+      </Input.Root>
 
       <MainNavigation />
 
