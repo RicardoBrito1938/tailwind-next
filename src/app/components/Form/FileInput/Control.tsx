26855966659f7442679e4d65451510ce85364b36
RicardoBrito1938 <ricardo.jucrist@hotmail.com>
2023-12-13 14:55:05 +0000

    feature: context preview and composition pattern for input

--- src/app/components/Form/FileInput/Control.tsx
+++ src/app/components/Form/FileInput/Control.tsx
@@ -1,7 +1,32 @@
-import { ComponentProps } from 'react'
+'use client'
+
+import { ChangeEvent, ComponentProps, useCallback } from 'react'
+import { useFileInput } from './Root'
 
 type ControlProps = ComponentProps<'input'>
 
 export const Control = (props: ControlProps) => {
-  return <input type="file" className="sr-only" id="photo" {...props} />
+  const { id, onFileSelected } = useFileInput()
+
+  const handleFileSelected = useCallback(
+    (event: ChangeEvent<HTMLInputElement>) => {
+      if (!event?.target?.files) {
+        return
+      }
+
+      const files = Array.from(event.target.files || [])
+      onFileSelected(files)
+    },
+    [onFileSelected],
+  )
+
+  return (
+    <input
+      type="file"
+      className="sr-only"
+      onChange={handleFileSelected}
+      id={id}
+      {...props}
+    />
+  )
 }
