5d69c457207ccfb69eb49176a44b53539b44c6f0
RicardoBrito1938 <ricardo.jucrist@hotmail.com>
2023-12-17 18:34:23 +0000

    feature: animating selects

--- src/app/components/Form/Select/index.tsx
+++ src/app/components/Form/Select/index.tsx
@@ -26,7 +26,7 @@ export const Select = ({ children, placeholder, ...props }: Props) => {
             sideOffset={8}
             side="bottom"
             position="popper"
-            className="z-10 w-[--radix-select-trigger-width] overflow-hidden rounded-lg border border-zinc-200 bg-white shadow-sm"
+            className=" animate-slideDownAndFade z-10 w-[--radix-select-trigger-width] overflow-hidden rounded-lg border border-zinc-200 bg-white shadow-sm"
           >
             <SelectPrimitive.Viewport>{children}</SelectPrimitive.Viewport>
           </SelectPrimitive.Content>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,22 @@ const config: Config = {
         profile: 'max-content 1fr min-content',
         form: 'minmax(7.5rem, 17.5rem) minmax(25rem, 1fr) minmax(0, 15rem)',
       },
+
+      keyframes: {
+        slideDownAndFade: {
+          from: { opacity: '0', transform: 'translateY(-2px)' },
+          to: { opacity: '1', transform: 'translateY(0)' },
+        },
+        slideUpAndFade: {
+          from: { opacity: '1', transform: 'translateY(0)' },
+          to: { opacity: '0', transform: 'translateY(-2px)' },
+        },
+      },
+
+      animation: {
+        slideDownAndFade: 'slideDownAndFade 0.3s cubic-bezier(0.4, 0, 0.2, 1)',
+        slideUpAndFade: 'slideUpAndFade 0.3s cubic-bezier(0.4, 0, 0.2, 1)',
+      },
     },
   },
   plugins: [],
